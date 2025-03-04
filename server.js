1c400ef536cda6026942d44467f500385ff9d0a3
MarsGotBars <marcinm@live.nl>
2025-03-04 15:33:09 +0100

    comments, logs and cleaning

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const exerciseList = await fetch(
   "https://fdnd-agency.directus.app/items/dropandheal_exercise"
 );
 // Skip hiermee het benoemen van 'data'
+// ! gebruik ik nog niet
 const { data: exerciseListJson } = await exerciseList.json();
 
 let taskData = [];
@@ -16,56 +17,60 @@ const fetchThemedTask = async () => {
     const { data: taskListJson } = await taskList.json();
 
     taskData = taskListJson.map(task => {
-      // in het geval dat de map fout gaat maken we deze array niet aan.
+      // In het geval dat de map fout gaat maken we deze array niet aan.
       const taskThemes = [
-        { theme: "blue", pathName: "verlies-aanvaarden" },
-        { theme: "red", pathName: "pijn-doorvoelen" },
-        { theme: "green", pathName: "verder-verandering" },
-        { theme: "purple", pathName: "emotioneel-verder" }
+        { taskTheme: "blue", pathName: "verlies-aanvaarden" },
+        { taskTheme: "red", pathName: "pijn-doorvoelen" },
+        { taskTheme: "green", pathName: "verder-verandering" },
+        { taskTheme: "purple", pathName: "emotioneel-verder" }
       ];
-      // offset aangezien ids starten bij 1 en de array bij 0
+
+      // offset aangezien ids starten bij 1 en de array bij 0, dus -1
       const themeIndex = task.id - 1;
+
+      // Bouw hier heb object op zoals we dat willen, met fallbacks als ik iets fout heb geschreven.
       return {
+        // Alle keys die al bestaan in het object
         ...task,
+        // En de keys die we zelf toevoegen
         pathName: taskThemes[themeIndex].pathName || `missing-name-${themeIndex}`,
-        theme: taskThemes[themeIndex].theme || `missing-color-${themeIndex}`,
+        taskTheme: taskThemes[themeIndex].taskTheme || `missing-color-${themeIndex}`,
       };
     });
-    
-    // Log themes after they're populated
-    // console.log("Themes fetched & enhanced:", taskData);
   } catch (error) {
     console.error("Error fetching themes:", error);
   }
 };
 
-fetchThemedTask();  // Initiate the fetch operation
+fetchThemedTask();
 const app = express();
 
 app.use(express.static("public"));
 
-// Stel Liquid in als 'view engine'
 const engine = new Liquid();
 app.engine("liquid", engine.express());
 
-// Stel de map met Liquid templates in
-// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
 app.set("views", "./views");
 
-// Maak een GET route voor de index (meestal doe je dit in de root, als /)
 app.get("/", async function (request, response) {
-  // console.log({ "tasklist": taskData });
-
   response.render("index.liquid");
 });
 
+// Dynamische parameter om te gebruiken bij het vinden van de correcte taskData
 app.get("/:theme", async function (request, response) {
+  // Sla deze op voor leesbaarheid
   const requestedTheme = request.params.theme
+
+  // Zoek taskData dmv de gevraagde :theme
   const foundData = taskData.find(data => data.pathName === requestedTheme)
-  console.log(foundData);
+
+  // Destructureer om props makkelijk door te
+  const {taskTheme, title} = foundData
   
+  // respond met de gevraagde pagina & het behorende thema
   response.render(`${foundData.pathName}.liquid`, {
-    themeColor: foundData.theme
+    taskTheme,
+    title
   });
 });
 
@@ -73,12 +78,8 @@ app.post("/", async function (request, response) {
   response.redirect(303, "/");
 });
 
-// Stel het poortnummer in waar Express op moet gaan luisteren
-// Lokaal is dit poort 8000, als dit ergens gehost wordt, is het waarschijnlijk poort 80
 app.set("port", process.env.PORT || 8000);
 
-// Start Express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get("port"), function () {
-  // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get("port")}`);
 });
