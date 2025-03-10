4b263b50415b42ca486d23f792a7723a7d642234
Marcin Magdziak <marcinm@live.nl>
2025-03-10 14:40:02 +0100

    #9 & async fetching

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
 import express from "express";
 import { Liquid } from "liquidjs";
 
-const exerciseList = await fetch(
-  "https://fdnd-agency.directus.app/items/dropandheal_exercise"
-);
-// Skip hiermee het benoemen van 'data'
-const { data: exerciseListJson } = await exerciseList.json();
-
 let taskData = [];
-
+let exerciseData = [];
 const fetchThemedTask = async () => {
   try {
-    const taskList = await fetch("https://fdnd-agency.directus.app/items/dropandheal_task");
+    const taskList = await fetch(
+      "https://fdnd-agency.directus.app/items/dropandheal_task"
+    );
     // Skip hiermee het benoemen van 'data'
     const { data: taskListJson } = await taskList.json();
 
-    taskData = taskListJson.map(task => {
+    taskData = taskListJson.map((task) => {
       // In het geval dat de map fout gaat maken we deze array niet aan.
+      // ! remove, was added to the database
       const taskThemes = [
         { taskTheme: "blue", pathName: "verlies-aanvaarden" },
         { taskTheme: "red", pathName: "pijn-doorvoelen" },
         { taskTheme: "green", pathName: "verder-verandering" },
-        { taskTheme: "purple", pathName: "emotioneel-verder" }
+        { taskTheme: "purple", pathName: "emotioneel-verder" },
       ];
 
       // offset aangezien ids starten bij 1 en de array bij 0, dus -1
@@ -32,16 +29,31 @@ const fetchThemedTask = async () => {
         // Alle keys die al bestaan in het object
         ...task,
         // En de keys die we zelf toevoegen
-        pathName: taskThemes[themeIndex].pathName || `missing-name-${themeIndex}`,
-        taskTheme: taskThemes[themeIndex].taskTheme || `missing-color-${themeIndex}`,
+        pathName:
+          taskThemes[themeIndex].pathName || `missing-name-${themeIndex}`,
+        taskTheme:
+          taskThemes[themeIndex].taskTheme || `missing-color-${themeIndex}`,
       };
-    });    
+    });
   } catch (error) {
     console.error("Error fetching themes:", error);
   }
 };
 
-fetchThemedTask();
+const fetchThemedExercise = async () => {
+  const exerciseList = await fetch(
+    "https://fdnd-agency.directus.app/items/dropandheal_exercise"
+  );
+  // Skip hiermee het benoemen van 'data'
+  const { data: exerciseListJson } = await exerciseList.json();
+  // Refresh de data voor de check
+  exerciseData = exerciseListJson;
+};
+
+(async () => {
+  await fetchThemedExercise();
+  await fetchThemedTask();
+})();
 const app = express();
 
 app.use(express.static("public"));
@@ -56,30 +68,35 @@ app.get("/", async function (request, response) {
 });
 
 // Dynamische parameter om te gebruiken bij het vinden van de correcte taskData
-app.get("/stap/:theme", async function (request, response) {
+app.get("/:theme", async function (request, response, next) {
   // Custom lijst met alle task titles en pathNames
-  const tasks = taskData.map(task => ({
+  const tasks = taskData.map((task) => ({
     title: task.title,
     pathName: task.pathName,
-    id: task.id
+    id: task.id,
   }));
   // Sla deze op voor leesbaarheid
-  const requestedTheme = request.params.theme
+  const requestedTheme = request.params.theme;
   // Zoek taskData dmv de gevraagde :theme
-  const foundData = taskData.find(data => data.pathName === requestedTheme)
+  const foundData = taskData.find((data) => data.pathName === requestedTheme);
+
+  if (!foundData) {
+    return response.status(404).render("err.liquid");
+  }  
+
   // Destructureer om props makkelijk door te
-  const {pathName,taskTheme, title, id, exercise: exerciseList} = foundData
-  
-  const exerciseData = exerciseList.map(exercise => {
-    return exerciseListJson.find(e => e.id === exercise)
+  const { pathName, taskTheme, title, id, exercise: exerciseList } = foundData;
+
+  const exerciseInfo = exerciseList.map((exercise) => {
+    return exerciseData.find((e) => e.id === exercise);
   });
-  const exercises = exerciseData.map(exercise =>{
+  const exercises = exerciseInfo.map((exercise) => {
     return {
       ...exercise,
       // absoluut pad, altijd handig -Krijn
-      pathName: `/stap/${pathName}/${exercise.id}`
-    }
-  })
+      pathName: `/${pathName}/${exercise.id}`,
+    };
+  });
 
   // respond met de gevraagde pagina & het behorende thema
   response.render(`task.liquid`, {
@@ -87,22 +104,29 @@ app.get("/stap/:theme", async function (request, response) {
     title,
     id,
     tasks,
-    exercises
+    exercises,
   });
 });
 
-app.get("/stap/:theme/:pageId", async function (request, response) {
-  const {theme, pageId} = request.params
-  const foundData = taskData.find(data => data.pathName === theme)
-  const exercise = exerciseListJson.find(exercise => exercise.id === parseInt(pageId))
-  const {title, description} = exercise
-  const {taskTheme, id} = foundData
+app.get("/:theme/:pageId", async function (request, response) {
+  const { theme, pageId } = request.params;
+  const foundData = taskData.find((data) => data.pathName === theme);
+  
+  // - 1 omdat we door een array lopend die start op 0
+  const exercise = exerciseData.find(
+    (exercise) => exercise.id === foundData.exercise[pageId - 1]
+  );
+  if (!exercise) {
+    return response.status(404).render("err.liquid");
+  }
+  const { title, description } = exercise;
+  const { taskTheme, id } = foundData;
   response.render(`exercise.liquid`, {
     taskTheme,
     title,
     description,
-    id
-  })
+    id,
+  });
 });
 
 app.post("/", async function (request, response) {
