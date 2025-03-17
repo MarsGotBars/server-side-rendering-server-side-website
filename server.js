51093c0f9dc53ef965a7e00407585632b434ae3c
Marcin Magdziak <marcinm@live.nl>
2025-03-17 11:24:48 +0100

    basic setup #9

--- server.js
+++ server.js
@@ -94,9 +94,9 @@ app.get("/:theme/:pageId", async function (request, response) {
     return response.status(404).render("err.liquid");
   }
   const { title, description, image } = exercise;
-  const { theme: foundTheme, id } = foundData;
+  const { foundtheme, id } = foundData;
   response.render(`exercise.liquid`, {
-    foundTheme,
+    foundtheme,
     title,
     description,
     id,
